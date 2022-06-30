b6e914ffe2dcc34847d6d3c40b2464949579bd92
euije <exitfill1234@naver.com>
2022-07-01 07:50:36 +0900

    Add 2nd files

--- 2nd study/Codes/_11279.java
+++ 2nd study/Codes/_11279.java
@@ -3,36 +3,44 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Collections;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
+
+// https://m.blog.naver.com/ka28/221850826909
+// https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html
+// https://coding-factory.tistory.com/603
 
 public class _11279 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        //StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
         
         int N = Integer.parseInt(st.nextToken());
         int input;
 
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             input = Integer.parseInt(st.nextToken());
+
             if(input == 0){
                 if(pq.isEmpty())
-                    System.out.println(0);
+                    result.append(String.valueOf(0) + "\n");
                 else {
-                    pq.peek();
-                    cout << heap.top() << "\n";
-                    heap.pop();
+                    result.append(String.valueOf(pq.poll()) + "\n");
                 }
             }
             else {
-                heap.push(input[i]);
+                pq.add(input);
             }
         }
-        
+
+        bw.write(result.toString());
+        bw.close();
+        br.close();
     }
 }
