18b5859d50cee537fdd4ae6c4a06ded72e9b6d00
CHAE <chaeseung-un@chaeseung-un-ui-MacBookAir.local>
2022-09-09 08:34:20 +0900

    9/8 chae

--- 2022 Fall/9:9/5014.cpp 채승운.cpp
+++ 2022 Fall/9:9/5014.cpp 채승운.cpp
@@ -17,24 +17,25 @@ string BFS(int F, int S, int G, int U, int D)
     {
         int now = q.front();
         q.pop();
-        if(now == G)
+        if(now == G) // 목표 층에 도착한 경우
         {
             return to_string(arr[now] - 1);
         }
-        if(now + U <= F)
+        if(now + U <= F) // 목표 층에 도달하지 않음 + 최고층 뚫지 않음
         {
-            if(arr[now + U] == 0){
+            if(arr[now + U] == 0){ // 최초 방문에만 값이 초기화되어야 함.
                 arr[now + U] = arr[now] + 1;
                 q.push(now + U);
             }
         }
-        if(now - D > 0)
+        if(now - D > 0) // 목표 층에 도달하지 않음 + 내려가야 하는 상황
         {
-            if(arr[now - D] == 0){
+            if(arr[now - D] == 0){ // 최초 방문에만 값이 초기화되어야 함.
                 arr[now - D] = arr[now] + 1;
                 q.push(now - D);
             }
         }
+        // 한 사이클마다 특정 층 + G층과 특정 층 - D층에 방문 횟수가 새겨짐
     }
     return "use the stairs";
 }
